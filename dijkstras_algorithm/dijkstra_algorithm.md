20d7fdf7175965c2a853d9520db1d3e2f7a584f7
Ryan Walker <rytwalker@gmail.com>
2019-07-24 17:42:09 -0400

    working on dijkstra algorithm

--- dijkstras_algorithm/dijkstra_algorithm.md
+++ dijkstras_algorithm/dijkstra_algorithm.md
@@ -11,3 +11,19 @@ Steps:
 2. Update the cost of the neighbors of this node.
 3. Repeat until done for every node on graph.
 4. Calculate final path.
+
+
+1. Find the "cheapest" node.
+   1. POSTER 0
+   2. LP 5
+   3. DRUM SET 20
+   4. BASS 15
+   5. PIANO 10
+2. Update the cost of the neighbors of this node.
+3. Repeat until done for every node on graph.
+4. Calculate final path.
+
+## 3 Hashtables for Dijkstra's Algorithm
+1. Graph
+2. Costs
+3. Parents
--- dijkstras_algorithm/dijkstras_algorithm.py
+++ dijkstras_algorithm/dijkstras_algorithm.py
@@ -15,6 +15,15 @@ graph["b"]["a"] = 3
 graph["b"]["fin"] = 5
 graph["fin"] = {}
 
+'''
+graph {
+    start: {a: 6, b: 2},
+    a: {fin: 1}
+    b: {a: 3, fin: 5}
+    fin: {}
+}
+'''
+
 # costs hash table
 infinity = float("inf")
 costs = {}
@@ -22,27 +31,48 @@ costs["a"] = 6
 costs["b"] = 2
 costs["fin"] = infinity
 
+
+'''
+costs: {a: 6, b: 2, fin: infinity}
+'''
+
 # parents hash table
 parents = {}
 parents["a"] = "start"
 parents["b"] = "start"
 parents["fin"] = None
 
+'''
+parents: {a: "start", b: "start", fin: None}
+'''
+
 processed = set()
 
-node = find_lowest_node(costs)
+
+def find_lowest_cost_node(costs):
+    lowest_cost = float("inf")
+    lowest_cost_node = None
+    for node in costs:
+        cost = costs[node]
+        if cost < lowest_cost and node not in processed:
+            lowest_cost = cost
+            lowest_cost_node = node
+        return lowest_cost_node
+
+
+node = find_lowest_cost_node(costs)
 # while we have nodes to process
 while node:
     # grab the node that's closest to the start
-    cost = costs[node]
-    neighbors = graph[node]
+    cost = costs[node]  # 2 5
+    neighbors = graph[node]  # {a: 3, fin: 5} {fin: 1}
     # update costs for its neighbors
     for n in neighbors.keys():
-        new_cost = cost + neighbors[n]
+        new_cost = cost + neighbors[n]  # 5 7 6
         # if any neightbors' costs were updated, update parents' too
         if costs[n] > new_cost:
-            costs[n] = new_cost
-            parents[n] = node
+            costs[n] = new_cost  # {a: 5} {fin: 7} {fin: 6}
+            parents[n] = node  # {a: 'b'} {fin: b} {fin: a}
     # mark this node processed
     processed.add(node)
-    node = find_lowest_node(node)
+    node = find_lowest_cost_node(node)
